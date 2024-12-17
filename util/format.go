e0c5a532a3a0585308a16c8c8398d7c83817e957
mrkonz <mrkonz@odds.team>
2024-12-17 12:25:48 +0700

    add money formart

---
+++ util/format.go
@@ -0,0 +1,23 @@
+package util
+
+import (
+	"fmt"
+	"strings"
+)
+
+func FormatMoney(num float64) string {
+	intPart := int64(num)
+	decPart := strings.Split(fmt.Sprintf("%.2f", num), ".")[1]
+	intStr := fmt.Sprintf("%d", intPart)
+
+	// Add commas
+	var result []string
+	for i, v := range intStr {
+		if i > 0 && (len(intStr)-i)%3 == 0 {
+			result = append(result, ",")
+		}
+		result = append(result, string(v))
+	}
+
+	return strings.Join(result, "") + "." + decPart
+}
